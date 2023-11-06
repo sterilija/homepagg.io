1052897ac8a1d7abbde047460c8a9a3d44ce7b12
WzrywProstaty <joint353@gmail.com>
2023-11-06 09:37:32 +0300

    initialized the project structure

---
+++ src/App.vue
@@ -0,0 +1,24 @@
+<template>
+  <v-app>
+    <v-main>
+      
+    </v-main>
+  </v-app>
+</template>
+
+<script lang="ts">
+import { defineComponent } from 'vue'
+
+
+export default defineComponent({
+  name: 'App',
+
+  components: {
+    
+  },
+
+  data:()=>({
+    //
+  })
+})
+</script>
